9d4ea142dc3a322d2d1011977f682fb8a3322ab3
kheeva <kheeva@ya.ru>
2017-12-09 20:25:14 +0300

    incomplete. argparse console ready.

--- image_resize.py
+++ image_resize.py
@@ -1,3 +1,4 @@
+import argparse
 
 
 def resize_image(path_to_original, path_to_result):
@@ -5,4 +6,24 @@ def resize_image(path_to_original, path_to_result):
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Resize an image.')
+
+    parser.add_argument('file', metavar='image file',
+                        help='path to an image file')
+    parser.add_argument('--scale', '-s', type=float,
+                        help='scale an image in s times')
+    parser.add_argument('--width', type=float, help='image width',
+                        )
+    parser.add_argument('--height', type=float, help='image height',
+                        )
+    parser.add_argument('--output', '-o', help='path to an output image')
+
+    args = parser.parse_args()
+
+    if not (args.scale or args.width or args.height):
+        parser.error(
+            'one of resize parameters must be given:'
+            ' [-s | {--width and/or --height}]')
+
+    if args.scale and (args.width or args.height):
+        parser.error('error: --width or --height not allowed with --scale.')
