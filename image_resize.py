defb0a7513865ec455795a991215e02be217726d
kheeva <kheeva@ya.ru>
2017-12-10 17:32:35 +0300

    incomplete. scale resizing have done.

--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,30 @@
 import argparse
+import os
+from PIL import Image
 
 
 def resize_image(path_to_original, path_to_result):
-    pass
+    image = Image.open(path_to_original)
+    if args.scale:
+        if args.scale > 0:
+            output_file_width, output_file_height = map(lambda x: x*args.scale,
+                                                        image.size)
+        else:
+            output_file_width, output_file_height = map(
+                                            lambda x: -1*x//args.scale,
+                                            image.size)
+
+        image = image.resize((output_file_width, output_file_height),
+                             Image.ANTIALIAS)
+
+    if args.output:
+        image.save(args.output)
+    else:
+        output_file_name, output_file_extension = os.path.basename(
+            file_name).split('.')
+        output_file = '{}__{}x{}.{}'.format(output_file_name, output_file_width,
+                                    output_file_height, output_file_extension)
+        image.save(output_file)
 
 
 if __name__ == '__main__':
@@ -10,12 +32,10 @@ if __name__ == '__main__':
 
     parser.add_argument('file', metavar='image file',
                         help='path to an image file')
-    parser.add_argument('--scale', '-s', type=float,
-                        help='scale an image in s times')
-    parser.add_argument('--width', type=float, help='image width',
-                        )
-    parser.add_argument('--height', type=float, help='image height',
-                        )
+    parser.add_argument('--scale', '-s', type=int,
+                        help='scale by a factor of s')
+    parser.add_argument('--width', type=int, help='image width')
+    parser.add_argument('--height', type=int, help='image height')
     parser.add_argument('--output', '-o', help='path to an output image')
 
     args = parser.parse_args()
@@ -27,3 +47,5 @@ if __name__ == '__main__':
 
     if args.scale and (args.width or args.height):
         parser.error('error: --width or --height not allowed with --scale.')
+
+    resize_image(args.file, args.output)
